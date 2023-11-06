92e4f06689f01ae2a4d366294c0f28df5dd63b0b
Ayush-1Kumar <129687797+Ayush-1Kumar@users.noreply.github.com>
2023-11-06 23:49:53 +0530

    commit

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,56 +1,101 @@
-import React from 'react';
+import React, { useState} from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useNavigate } from 'react-router-dom';
+
+
 
 
 const Signup = () => {
-  const backgroundImageStyle = {
-    backgroundImage: "url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp')",
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
 
-  const containerStyle = {
-    borderRadius: '15px',
-  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  const gradientCustom3 = {
-    background: "#84fab0",
-    background: "-webkit-linear-gradient(to right, rgba(132, 250, 176, 0.5), rgba(143, 211, 244, 0.5))",
-    background: "linear-gradient(to right, rgba(132, 250, 176, 0.5), rgba(143, 211, 244, 0.5))"
-  };
-
-  const gradientCustom4 = {
-    background: "#84fab0",
-    background: "-webkit-linear-gradient(to right, rgba(132, 250, 176, 1), rgba(143, 211, 244, 1))",
-    background: "linear-gradient(to right, rgba(132, 250, 176, 1), rgba(143, 211, 244, 1))"
+    fetch('http://localhost:8080/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Response from server:', data);
+        navigate('/login');
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   };
 
   return (
-    <section className="vh-100 bg-image" style={backgroundImageStyle}>
+    <section className="vh-100 bg-image">
       <div className="mask d-flex align-items-center h-100 gradient-custom-3">
         <div className="container h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
             <div className="col-12 col-md-9 col-lg-7 col-xl-6">
-              <div className="card" style={containerStyle}>
+              <div className="card" style={{ borderRadius: '15px' }}>
                 <div className="card-body p-5">
                   <h2 className="text-uppercase text-center mb-5">Create an account</h2>
 
-                  <form action='./login' method='post'>
+                  <form onSubmit={handleSubmit} method='post'>
                     <div className="form-outline mb-4">
-                      <input type="text" id="form3Example1cg" className="form-control form-control-lg"  name="name"/>
+                      <input
+                        type="text"
+                        id="form3Example1cg"
+                        className="form-control form-control-lg"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                      />
                       <label className="form-label" htmlFor="form3Example1cg">Your Name</label>
                     </div>
 
                     <div className="form-outline mb-4">
-                      <input type="email" id="form3Example3cg" className="form-control form-control-lg" name="email" />
+                      <input
+                        type="email"
+                        id="form3Example3cg"
+                        className="form-control form-control-lg"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                      />
                       <label className="form-label" htmlFor="form3Example3cg">Your Email</label>
                     </div>
 
                     <div className="form-outline mb-4">
-                      <input type="password" id="form3Example4cg" className="form-control form-control-lg" name="password" />
+                      <input
+                        type="password"
+                        id="form3Example4cg"
+                        className="form-control form-control-lg"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                      />
                       <label className="form-label" htmlFor="form3Example4cg">Password</label>
                     </div>
 
                     <div className="form-outline mb-4">
-                      <input type="password" id="form3Example4cdg" className="form-control form-control-lg" />
+                      <input
+                        type="password"
+                        id="form3Example4cdg"
+                        className="form-control form-control-lg"
+                        name="confirmPassword"
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                      />
                       <label className="form-label" htmlFor="form3Example4cdg">Repeat your password</label>
                     </div>
 
@@ -62,7 +107,7 @@ const Signup = () => {
                     </div>
 
                     <div className="d-flex justify-content-center">
-                      <button type="submit" className="btn btn-success btn-block btn-lg" style={gradientCustom4}>Register</button>
+                      <button type="submit" className="btn btn-success btn-block btn-lg" style={{ background: 'linear-gradient(to right, rgba(132, 250, 176, 1), rgba(143, 211, 244, 1))' }}>Register</button>
                     </div>
 
                     <p className="text-center text-muted mt-5 mb-0">Have already an account? <a href="/login" className="fw-bold text-body"><u>Login here</u></a></p>
--- src/backend/app.js
+++ src/backend/app.js
@@ -1,27 +1,32 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const app = express();
-const path = require('path');
-const usermodel =require("./models/usermodel");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const usermodel = require("./models/usermodel");
 
-app.use(express.urlencoded({extended:true}));
-app.use(express.json())
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 mongoose.connect("mongodb://localhost:27017/typing", { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("mongo is connected");
+db.once('open', function () {
+  console.log("MongoDB is connected");
 });
 
-app.post('./login',async(req,res)=>{
-    const data=req.body;
-    const user= await new usermodel(data);
+app.post('./login', async (req, res) => {
+  const data = req.body;
+  try {
+    const user = new usermodel(data);
     await user.save();
-    res.send('successfully')
+    res.json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-})
 app.listen(8080, () => {
-    console.log("server is running on port:3000");
-  });
-  
+  console.log("Server is running on port: 8080");
+});
