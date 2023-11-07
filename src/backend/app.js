71091b158cc7be0245aa4ee31b53abd7b9beee52
Ayush-1Kumar <129687797+Ayush-1Kumar@users.noreply.github.com>
2023-11-07 16:23:00 +0530

    commit

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { body, validationResult } = require('express-validator');
 const usermodel = require("./models/usermodel");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 
@@ -16,36 +18,67 @@ db.once('open', function () {
   console.log("MongoDB is connected");
 });
 
-app.post('/signup', async (req, res) => {
+app.post('/signup', [
+  body('email').isEmail(),
+  body('name').isLength({ min: 5 }),
+  body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   const data = req.body;
+
   try {
-    const user = new usermodel(data);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const user = new usermodel({
+      name: data.name,
+      email: data.email,
+      password: hashedPassword
+    });
+
     await user.save();
     res.json({ message: 'User registered successfully' });
   } catch (error) {
-   res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-app.post('/login', async (req, res) => {
-  
+app.post('/login', [
+  body('email').isEmail(),
+  body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   let email = req.body.email;
   const password = req.body.password;
+
   try {
     let userData = await usermodel.findOne({ email });
-    
+
     if (!userData) {
       return res.status(500).json({ error: "User not found" });
     }
-    if (password !== userData.password) {
+
+    const isPasswordValid = await bcrypt.compare(password, userData.password);
+
+    if (!isPasswordValid) {
       return res.status(500).json({ error: "Incorrect password" });
     }
+
     res.json({ success: true, message: 'Login successful' });
   } catch (error) {
     console.log("Error:", error);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+
 
 app.listen(8080, () => {
   console.log("Server is running on port: 8080");
