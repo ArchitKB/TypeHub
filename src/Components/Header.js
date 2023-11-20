ac2ca00333fa81369970c3ea0c0be5f26ba03316
i-ayush-singh <singh.ayush.6262@gmail.com>
2023-11-20 14:19:27 +0530

    Made changes in styling of homepage

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,45 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
 import logo from "./logo-removebg-preview.png";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import Body from "./Body";
-
+import "./login.css"
 function Header() {
-  
+  // Animation for the logo
+  const logoAnimation = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 1000 },
+  });
+
+  // Animation for the navigation links
+  const linksAnimation = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 1000, delay: 200 },
+  });
 
   return (
     <div>
       <header className="header">
-        <img className="logo" src={logo} alt="logo" />
-        <h1>TypeHub</h1>
+        <animated.img
+          className="logo"
+          src={logo}
+          alt="logo"
+          style={logoAnimation}
+        />
+        <animated.h1 style={logoAnimation}>TypeHub</animated.h1>
 
         <div className="right-links">
-          <Link className="right" to="/signup">Signup</Link>
-          <Link className="right" to="/login">Login</Link>
+          <animated.div style={linksAnimation} className="right">
+            <Link to="/signup"><button className="btn ">SignUp</button></Link>
+          </animated.div>
+          <animated.div style={linksAnimation} className="right">
+            <Link to="/login"><button className="btn ">Login</button></Link>
+          </animated.div>
         </div>
       </header>
-      <Body/>
-
+      <Body />
     </div>
   );
 }
--- src/fontend/Landing.js
+++ src/fontend/Landing.js
@@ -1,18 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSpring, animated } from 'react-spring';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Landing.css'; // Import a separate CSS file for styling
+import Header from './Header2';
+import image from './img.jpg';
+// import placeholderImage1 from './placeholder1.jpg';
+// import placeholderImage2 from './placeholder2.jpg';
 
 const Landing = () => {
+  // Animation for the main heading
+  const headingAnimation = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 1000 },
+  });
+
+  // Animation for the card1
+  const card1Animation = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 1000, delay: 200 },
+  });
+
+  // Animation for the card2
+  const card2Animation = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 1000, delay: 400 },
+  });
+
   return (
     <div className="landing-container">
-      <h1 className="text-center mb-5">Typing Master</h1>
+      <animated.h1 style={headingAnimation} className="text-center mb-5">
+        <Header />
+      </animated.h1>
 
       <div className="row">
-        {/* Guest Mode */}
+        {/* Image */}
         <div className="col-md-6 mb-4">
+          <animated.img
+            src={image}
+            alt="typing"
+            className="custom-image"
+            style={card1Animation}
+          />
+        </div>
+
+        {/* Guest Mode */}
+        <animated.div className="col-md-6 mb-4 card1" style={card1Animation}>
           <div className="card">
-            <div className="card-body text-center">
+            <div className="card-body text-left box1">
               <h5 className="card-title">Guest Mode</h5>
               <p className="card-text">
                 Improve your typing skills without creating an account.
@@ -22,12 +60,12 @@ const Landing = () => {
               </Link>
             </div>
           </div>
-        </div>
+        </animated.div>
 
         {/* User Mode */}
-        <div className="col-md-6 mb-4">
+        <animated.div className="col-md-6 mb-4 card2" style={card2Animation}>
           <div className="card">
-            <div className="card-body text-center">
+            <div className="card-body text-center box2">
               <h5 className="card-title">User Mode</h5>
               <p className="card-text">
                 Track your progress and save your typing history by logging in.
@@ -40,8 +78,18 @@ const Landing = () => {
               </Link>
             </div>
           </div>
-        </div>
+        </animated.div>
       </div>
+
+      {/* Additional Images */}
+      {/* <div className="row">
+        <div className="col-md-6 mb-4">
+          <img src={placeholderImage1} alt="Placeholder 1" className="custom-image" />
+        </div>
+        <div className="col-md-6 mb-4">
+          <img src={placeholderImage2} alt="Placeholder 2" className="custom-image" />
+        </div>
+      </div> */}
     </div>
   );
 };
